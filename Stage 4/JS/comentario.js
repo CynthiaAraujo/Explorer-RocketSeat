a903542a7115d4635475e89fed0d4941e95f4bce
Cynthia Araújo <cynthia.araujo@ee.ufcg.edu.br>
2022-08-23 19:35:34 -0300

    First commit

---
+++ Stage 4/JS/comentario.js
@@ -0,0 +1,13 @@
+
+// Comentario de uma linha
+console.log("Bem vindo")
+/* Comentario de um bloco */
+
+//String - Pode ser '' (aspas simples)
+// "" - (aspas Duplas)
+// ``(Crase -  da para ter multi linhas, pular e colocar expressao de linguagem)
+console.log("Ola 'Ola2'")
+console.log(`Joao foi 
+ao supermercado ${1+1}
+s
+`)
