7e8ba827c92257a39a77d180a975766e38c32885
Cynthia Araújo <cynthia.araujo@ee.ufcg.edu.br>
2022-08-23 22:31:48 -0300

    Discover desafio 4

--- Stage 4/JS/comentario.js
+++ Stage 4/JS/comentario.js
@@ -11,3 +11,76 @@ console.log(`Joao foi
 ao supermercado ${1+1}
 s
 `)
+
+/* Numeros
+ 33/ inteiros
+ 12.5// reais -float
+ NaN // Not a Number (exemplo - dividir um número por uma string);
+ Infinity // infinito (tem que escrever maiusculo o primeiro I)
+ */
+
+ console.log(Infinity)
+ console.log(33+24)
+console.log(12.5/2)
+
+/* Boolean
+
+  *somente 2 valores
+    true // verdadeiro
+    false //falso
+
+*/
+
+console.log(false)
+
+/* Undefined 
+  - Indefinido.
+  - Não existe
+
+  Null
+  - nulo
+  - objeto que não possui nada dentro
+  - diferente de undefined
+*/
+
+console.log(undefined =! null)
+console.log(undefined === null)
+
+
+/* Object 
+ - objeto
+ - Propriedades / atributos;
+ - Funcionalidade/ métodos;
+
+ {propriedade: "valor" }
+*/
+
+/* Array 
+ -lista
+ - agrupamento de dados
+- Criar um array : console.log(["Leite", "Ovos"]);
+ */
+
+
+/* ##Primitivos: (imutável)
+ - String; - "ABC" "BCA" "bca" "bCa"
+- Number;
+ - Boolean;
+ - Undefined;
+ - Symbol;
+ - BigInt;
+
+ ##Estruturais:
+  - Object;
+      *Array;
+      *map;
+      *set;
+      *Date;
+      *...
+  - Fuction:
+
+## Primitivo Estrutural:
+ - null;
+
+*/
+
---
+++ Stage 4/JS/script.js
@@ -0,0 +1,8 @@
+console.log({name: "mayk", idade: 30, 
+andar: function(){
+  console.log('andar')
+}
+})
+
+
+console.log(["Leite", "Ovos"])
